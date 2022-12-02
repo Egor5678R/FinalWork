e8af3e0be8eb98627488dd15847c4cae40713f81
Vasilev Egor <egorvasilev8087@gmail.com>
2022-12-02 14:36:51 +0300

    Two commit

--- c_sharp/FinalWork/finalwrk/Finalwrk.cs
+++ c_sharp/FinalWork/finalwrk/Finalwrk.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int count = 0;
+
+int countLess(string[] input, int n) 
+{
+    for(int i = 0; i < input.Length; i++) 
+    {
+        if(input[i].Length <= n) 
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+string[] findLess(string[] input, int n) 
+{
+    string[] output = new string[countLess(input, n)];
+    for(int i = 0, j = 0; i < input.Length; i++) 
+    {
+        if(input[i].Length <= n) 
+        {
+            output[j] = input[i];
+            j++;
+        }
+    }
+    return output;
+}
+
+string[] Array() 
+{
+    Console.Write("Введите значения через пробел: ");
+    return Console.ReadLine().Split(" ");
+}
+
+string[] array = Array();
+string[] result = findLess(array, 3);
+Console.WriteLine($"[{string.Join(", ", array)}] \n|\nv \n[{string.Join(", ", result)}]");
